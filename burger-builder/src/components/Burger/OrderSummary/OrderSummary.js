de9cde160f7cffb368eea3c585736c2b5d7e2c92
Ravi-S-Parsana <raviparsana@gmail.com>
2018-06-29 18:03:50 -0400

    1. Fixed redirecting. 2. Shared validation and other reusable components throughout application. 3. Used Environment variables for securing app. 4. Implemented Lazy Loading for checkout, orders and signup components.

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,10 +4,6 @@ import Ahoc from '../../../hoc/Ahoc/Ahoc';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    // This could be a functional component, doesn't have to be a class
-    componentWillUpdate() {
-
-    }
 
     render () {
         const ingredientSummary = Object.keys( this.props.ingredients )
--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import UserInfo from './UserInfo/UserInfo';
-import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 
